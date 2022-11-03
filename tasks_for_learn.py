e17dad7c9b9d48a882f1e758576e8c8b9a8d5bb2
Viktor-Kiptev <viktor-kiptev@hotmail.com>
2022-11-03 15:22:40 +0100

    Tests

--- tasks_for_learn.py
+++ tasks_for_learn.py
@@ -204,3 +204,5 @@ Suppose the following input is supplied to the program: without,hello,bag,world
 Then, the output should be: bag,hello,without,world
 Hint: In case of input data being supplied to the question, it should be assumed to be a console input.
 '''
+
+
