abfa5668733d5999057b91480b368d2a1bc41e3c
Viktor-Kiptev <viktor-kiptev@hotmail.com>
2022-11-13 19:17:17 +0100

    15

--- tasks_for_learn.py
+++ tasks_for_learn.py
@@ -336,3 +336,39 @@ DIGITS 3
 # print(counter)
 
 # Question 14
+'''
+Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+Suppose the following input is supplied to the program: Hello world!
+Answer: 
+UPPER CASE 1
+LOWER CASE 9
+'''
+# my_input = input('Enter data:\n')
+
+# def summ_upper_low(some_str):
+#     upper_case = 0
+#     lower_case = 0
+#     for i in some_str:
+#         if i.isalpha() and i.islower():
+#             lower_case += 1
+#         elif i.isalpha() and i.isupper():
+#             upper_case += 1
+#     print(f'Upper case {upper_case} \nLower case {lower_case}')
+#
+# summ_upper_low(my_input)
+
+# Question 15
+'''
+Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+Suppose the following input is supplied to the program: 9
+Then, the output should be: 11106
+'''
+# from functools import reduce
+#
+# x = input('please enter a digit:')
+# print(reduce(lambda x, y: int(x) + int(y), [x*i for i in range(1,5)]))
+#
+# sum = 0
+# for i in range(1,5):
+#     sum = sum + int(x*i)
+# print(sum)
