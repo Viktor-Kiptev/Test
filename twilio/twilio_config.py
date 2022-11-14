ec0b07e33cb1983556c527401669caa98f03bbb0
Viktor-Kiptev <viktor-kiptev@hotmail.com>
2022-11-14 15:30:41 +0100

    SMS_twilio

--- twilio/twilio_config.py
+++ twilio/twilio_config.py
@@ -1,4 +1,4 @@
 acc_sid = 'AC9ab4d5e24c8244de2dfe2216ef4c3649'
-auth_token = '07ffdf916af422bcf56caf730bede709'
+auth_token = ''
 mes_ser_sid = 'MG325dee379a39e6c3d2b76c4d3fc3cff6' #+13393453314
 my_phone = ''
