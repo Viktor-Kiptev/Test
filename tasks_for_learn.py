06b9b177df8f7e15df81387e1c76826ce4451ad0
Viktor-Kiptev <viktor-kiptev@hotmail.com>
2022-11-15 19:58:32 +0100

    day 5

--- tasks_for_learn.py
+++ tasks_for_learn.py
@@ -372,3 +372,55 @@ Then, the output should be: 11106
 # for i in range(1,5):
 #     sum = sum + int(x*i)
 # print(sum)
+
+# Question 16
+
+'''
+Use a list comprehension to square each odd number in a list. 
+The list is input by a sequence of comma-separated numbers. 
+Suppose the following input is supplied to the program: 1,2,3,4,5,6,7,8,9
+Then, the output should be: 1,9,25,49,81
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+'''
+
+# our_list = input().split(',')
+# shown_list =[]
+# for i in our_list:
+#     if int(i) % 2 !=0:
+#         shown_list.append(str(pow(int(i),2)))
+# print(','.join(shown_list))
+#
+# lst = [str(int(i)**2) for i in input().split(',') if int(i)%2!=0]
+# print(','.join(lst))
+
+# Question 17
+
+'''
+Write a program that computes the net amount of a bank account
+ based a transaction log from console input. 
+ The transaction log format is shown as following: D 100 W 200
+D means deposit while W means withdrawal.
+Suppose the following input is supplied to the program:
+D 300
+D 300
+W 200
+D 100
+Then, the output should be: 500
+Hints: In case of input data being supplied to the question, 
+it should be assumed to be a console input.
+'''
+
+
+# def user_transactions():
+#     total = 0
+#     while True:
+#         x = input().split()
+#         if not x:
+#             break
+#         type, value = map(str, x)
+#         if type =='D':
+#             total +=int(value)
+#         else:
+#             total -=int(value)
+#     print(total)
+# user_transactions()
