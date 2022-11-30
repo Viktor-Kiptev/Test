ce7cc9b50107fa4e424aa1269ecee541946acbb5
Viktor-Kiptev <viktor-kiptev@hotmail.com>
2022-11-30 17:40:16 +0100

    22

--- tasks_for_learn.py
+++ tasks_for_learn.py
@@ -620,9 +620,58 @@ it should be assumed to be a console input.
 # for key, value in some_dict.items():
 #     print(key, value, sep=':')
 
-# Quesztion 23
+# Question 23
 
 '''
 Write a method which can calculate square value of number.
 Hints: Using the ** operator which can be written as n**p where means n^p
 '''
+
+# def calculate_square(n, p):
+#     return n ** p
+# print(calculate_square(4, 4))
+
+# Question 24
+
+'''
+Question:
+Python has many built-in functions, and if you do not know how to use it,
+ you can read document online or find some books. 
+But Python has a built-in document function for every built-in functions.
+
+Please write a program to print some Python built-in functions documents,
+ such as abs(), int(), raw_input()
+
+And add document for your own function
+
+Hints:
+The built-in document method is __doc__
+'''
+
+# print(abs.__doc__)
+# print(int.__doc__)
+# print(map.__doc__)
+
+# Question 25
+
+'''
+Define a class, which have a class parameter and have a same instance parameter.
+'''
+
+
+# class Car:
+#     name = 'Car'
+#
+#     def __init__(self, name = None):
+#         self.name = name
+#
+#
+# ford = Car('Edge')
+#
+# print('%s name is %s' % (Car.name, ford.name))
+#
+# vw = Car()
+# vw.name = 'Golf'
+#
+# print('%s name is %s' % (Car.name, vw.name))
+
